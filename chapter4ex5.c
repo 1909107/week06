224fb71bfce94637bdaf29fcf290763d04f24878
AngeL Lugo <83649404+1909107@users.noreply.github.com>
2021-06-15 17:31:59 -0500

    Create chapter4ex5.c

---
+++ chapter4ex5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void){
+  // Aqui podemos imprimir un número de punto flotante utilizando la conversión %d//
+float fp;
+fp = 7.79;
+
+printf("Float is %d\n", fp);
+
+  //Aqui podemos imprimir un entero utilizando la conversión %f//
+int inte;
+inte = 4;
+
+printf("Integer is %f\n",inte);
+
+  //Aqui podemos imprimir un carácter utilizando la conversión %d//
+
+char cha ='a';
+printf("Character is %d", cha);
+
+}
