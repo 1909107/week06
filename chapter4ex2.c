b3a504adf161e2a88e96c07f6c3251bc89da9300
AngeL Lugo <83649404+1909107@users.noreply.github.com>
2021-06-15 17:05:28 -0500

    Create chapter4ex2.c

---
+++ chapter4ex2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){ 
+//Con este programa podremos imprimir la letra E con el simbolo de *//
+printf(" ******\n **\n **\n ******\n **\n **\n ******\n");
+
+ return 0;
+
+  }
