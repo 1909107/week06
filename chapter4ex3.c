f544698bb2316544c24d662d3820a0a91879c9e1
AngeL Lugo <83649404+1909107@users.noreply.github.com>
2021-06-15 17:59:48 -0500

    Update chapter4ex3.c

--- chapter4ex3.c
+++ chapter4ex3.c
@@ -10,11 +10,6 @@ Area = Anchura*Altura;
 
 
 printf ("el área de un rectángulo de 3 por 5 pulgadas es %.2d y el perimetro es %.2d pulgadas", Area, perimetro);
-
-
-// para que este programa funcione con una anchura de 6,8 y una altura de 2,3 pulgadas, hay que usar float
-// loat de área, unperímetro, un ancho, un  alto;
-// y para que se muestren los decimales usa %.2f //
  return 0;
 
   } 
