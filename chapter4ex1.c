c76819af3756c350d50c53acf4dc79585c121b13
AngeL Lugo <83649404+1909107@users.noreply.github.com>
2021-06-15 17:03:22 -0500

    Create chapter4ex1.c

---
+++ chapter4ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//Con este programa podremos imprimir nuestros datos basicos//
+int main(void){ 
+    printf("Name: Angel Abraham Lugo Saenz\n");
+    printf("Numero de seguridad social: 908364452\n");
+    printf("Fecha de nacimiento: 02/11/1997\n");
+ return 0;
+
+  }
